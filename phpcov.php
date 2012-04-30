8a66f654774946b81735b89318fcea1586381346
Sebastian Bergmann <sb@sebastian-bergmann.de>
2012-04-30 11:58:23 +0200

    List The BSD 3-Clause License correctly and consistently. Thanks to Christian Aistleitner <christian@quelltextlich.at> for bringing this issue to my attention.

--- phpcov.php
+++ phpcov.php
@@ -38,7 +38,7 @@
  * @package   PHP_CodeCoverage
  * @author    Sebastian Bergmann <sb@sebastian-bergmann.de>
  * @copyright 2011-2012 Sebastian Bergmann <sb@sebastian-bergmann.de>
- * @license   http://www.opensource.org/licenses/bsd-license.php  BSD License
+ * @license   http://www.opensource.org/licenses/BSD-3-Clause  The BSD 3-Clause License
  * @since     File available since Release 1.0.0
  */
 
