4c1572266ec42ea75a1d6d86157a4a34f32748ca
Sebastian Bergmann <sebastian@thephp.cc>
2016-01-09 06:25:16 +0100

    Prepare release

--- src/Application.php
+++ src/Application.php
@@ -24,7 +24,7 @@ class Application extends AbstractApplication
 {
     public function __construct()
     {
-        $version = new Version('2.0.2', dirname(__DIR__));
+        $version = new Version('3.0.0', dirname(__DIR__));
         parent::__construct('phpcov', $version->getVersion());
 
         $this->add(new ExecuteCommand);
