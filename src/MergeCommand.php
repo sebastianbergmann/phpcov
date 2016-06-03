873806cacab98de9479b7bb010b73c2d75d755d4
Webysther Nunes <Webysther@users.noreply.github.com>
2016-06-03 19:57:21 -0300

    --text as optional

--- src/MergeCommand.php
+++ src/MergeCommand.php
@@ -62,7 +62,7 @@ class MergeCommand extends BaseCommand
             ->addOption(
                 'text',
                 null,
-                InputOption::VALUE_NONE,
+                InputOption::OPTIONAL,
                 'Generate code coverage report in text format'
             )
             ->addOption(
