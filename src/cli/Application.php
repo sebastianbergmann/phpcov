f4cd73517718c0978b941711baea9f45701c5ced
Sebastian Bergmann <sb@sebastian-bergmann.de>
2020-10-01 11:21:51 +0200

    Bump

--- src/cli/Application.php
+++ src/cli/Application.php
@@ -15,7 +15,7 @@ use SebastianBergmann\Version;
 
 final class Application
 {
-    private const VERSION = '8.1.2';
+    private const VERSION = '8.2';
 
     public function run(array $argv): int
     {
