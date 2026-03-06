9f40cf67048425174326244228512950cc7a613e
Filippo Tessarotto <zoeslam@gmail.com>
2026-03-06 12:58:54 +0100

    Update test data set to new PHPUnit version

--- tests/fixture/example2/tests/Greeter2Test.php
+++ tests/fixture/example2/tests/Greeter2Test.php
@@ -1,5 +1,12 @@
-<?php
-
+<?php declare(strict_types=1);
+/*
+ * This file is part of phpcov.
+ *
+ * (c) Sebastian Bergmann <sebastian@phpunit.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 namespace fixture\example2\tests;
 
 use fixture\example2\src\Greeter2;
@@ -12,6 +19,6 @@ final class Greeter2Test extends TestCase
 {
     public function testGreetsWorld(): void
     {
-        $this->assertSame(Greeter2::GREETING, (new Greeter2())->greetWorld());
+        $this->assertSame(Greeter2::GREETING, (new Greeter2)->greetWorld());
     }
 }
